362889034f4c12386750e94621b0a4ed5dc309b3
Chanchal Saini <chanchalsaini3103@gmail.com>
2025-04-29 15:03:38 +0530

    🔧 Updated API URL to live Render backend

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+      <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        {/* Future: <Route path="/dashboard" element={<Dashboard />} /> */}
+
+        <Route path="/dashboard" element={<Dashboard />} /> 
       </Routes>
     </BrowserRouter>
   );
